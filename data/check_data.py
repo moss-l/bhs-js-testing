ccc2b29738111954673fbf7595f6027ee455551f
Peter Seibel <peter@gigamonkeys.com>
2021-10-10 10:15:42 -0700

    Order problem sets.

--- data/check_data.py
+++ data/check_data.py
@@ -5,18 +5,32 @@ import json
 with open("tests.json") as f:
     tests = json.load(f)
 
-with open("problem-sets.json") as f:
-    problem_sets = json.load(f)
+with open("problems.json") as f:
+    data = json.load(f)
+    sets = set(data['sets'])
+    problems = data['problems']
 
 
-in_problem_sets = { item for v in problem_sets.values() for item in v }
+have_problems = set(problems.keys())
+in_problems = { item for v in problems.values() for item in v }
 in_tests = set(tests.keys())
 
-if in_problem_sets == in_tests:
+if sets == have_problems:
+    print(f"All {len(sets)} named sets have problems.")
+else:
+    if x := (sets - have_problems):
+        print(f"Sets with no problems: {x}")
+
+    if x := (have_problems - sets):
+        print(f"Unused problem sets: {x}")
+
+
+
+if in_problems == in_tests:
     print(f"All {len(in_tests)} tests in both files.")
 else:
-    if x := (in_problem_sets - in_tests):
+    if x := (in_problems - in_tests):
         print(f"Only in problem sets: {x}")
 
-    if x := (in_tests - in_problem_sets):
+    if x := (in_tests - in_problems):
         print(f"Only in tests: {x}")
