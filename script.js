4e03338c72c4ec07fbb3f8a9369a389ca1146dd5
moss-l <mossleloverro@students.berkeley.net>
2025-08-15 18:41:03 +0000

    unfinished

--- script.js
+++ script.js
@@ -17,3 +17,31 @@ function countEvens(array){
     return count;
 
 }
+
+
+function maxBlock(s) {
+
+    let largestBlock = 0;
+    
+    for(let i = 0; i < s.length; i++){
+
+        let inChain = true;
+        let j = 0;
+
+        while(inChain){
+
+            inChain = s.substring(i, i+1).equals(s.substring(i+j, i+j+1));
+            
+            j++;
+            
+            console.log(j + " " + s.substring(i, i+1) + " " + s.substring(i+j, i+j+1));
+
+        }
+
+        if(j>largestBlock) largestBlock = j;
+
+    }
+
+    return largestBlock;
+
+}
